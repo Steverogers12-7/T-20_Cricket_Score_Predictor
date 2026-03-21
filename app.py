4048df04a8c8627aec24d7ab564f5a6816d717e2
Steve_rogers12 <9aashutosh6086@gmail.com>
2026-03-21 09:25:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,15 +26,12 @@ cities = ['Colombo','Mirpur','Johannesburg','Dubai','Auckland','Cape Town',
           'Mount Maunganui','Chittagong','Kolkata','Lahore','Delhi',
           'Nagpur','Chandigarh','Adelaide','Bangalore','St Kitts',
           'Cardiff','Christchurch','Trinidad']
-
-# ---------------- UI ----------------
 st.title('🏏 Cricket Score Predictor')
 
 col1, col2 = st.columns(2)
 
 with col1:
     batting_team = st.selectbox('Batting Team', sorted(teams))
-
 with col2:
     bowling_team = st.selectbox('Bowling Team', sorted(teams))
 
@@ -44,45 +41,37 @@ col3, col4, col5 = st.columns(3)
 
 with col3:
     current_score = st.number_input('Current Score')
-
 with col4:
-    overs_done = st.number_input('Overs Done (min 5)')
-
+    overs = st.number_input('Overs Done (works for over>5)')
 with col5:
     wickets = st.number_input('Wickets Out')
 
 last_five = st.number_input('Runs in Last 5 Overs')
 
-# ---------------- PREDICTION ----------------
 if st.button('Predict Score'):
 
     if batting_team == bowling_team:
         st.error("Batting and Bowling team must be different")
-
-    elif overs_done <= 0:
-        st.error("Overs must be greater than 0")
-
     else:
-        runs_left = 120 - current_score
-        balls_left = 120 - (overs_done * 6)
+        
+        balls_left = 120 - (overs * 6)
         wickets_left = 10 - wickets
 
-        crr = current_score / overs_done
-        rrr = runs_left / (balls_left / 6)
+        crr = current_score / overs
+        
 
         input_df = pd.DataFrame({
-    'batting_team':[batting_team],
-    'bowling_team':[bowling_team],
-    'city':[city],
-    'runs_left':[runs_left],
-    'balls_left':[balls_left],
-    'wickets_left':[wickets_left],
-    'total_runs':[current_score],   # ✅ FIX NAME
-    'crr':[crr],
-    'rrr':[rrr],
-    'last_five':[last_five]         # ✅ ADD
-})
+            'batting_team':[batting_team],
+            'bowling_team':[bowling_team],
+            'city':[city],
+            'current_score':[current_score],
+            'balls_left':[balls_left],
+            'wickets_left':[wickets],
+            'crr':[crr],
+            'last_five':[last_five]
+        })
 
         result = pipe.predict(input_df)
 
         st.success(f"Predicted Score: {int(result[0])}")
+
